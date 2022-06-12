fd084f66143afef23c2af5c20d09eae8408dc2a9
Ekaterina Dzeraeva <katja.dzeraeva@gmail.com>
2022-06-12 14:22:57 +0200

    created active records validations for models and chash tested them

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,2 +1,7 @@
 class Chef < ApplicationRecord
+  has_many :events, dependent: :destroy
+  has_many :restaurants, through: :events
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :stars, numericality: { in: 0..5}
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :comment, presence: true
+  validates :rating, numericality: { in: 0..5 }
 end
